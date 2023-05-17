c267120db8f3d7c0afe6ce448f2d02e52772ccda
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-05-17 16:26:26 -0600

    Partial change to author_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, foreign_key: :user_id, class_name: 'User'
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
   has_many :comments
   has_many :likes
   def update_posts_counter
@@ -7,6 +7,6 @@ class Post < ApplicationRecord
   end
 
   def recent_comments
-    comments.where(user_id: id).limit(5).order(created_at: :desc)
+    comments.where(post_id: id).limit(5).order(created_at: :desc)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   def recent_posts
-    posts.where(user_id: id).limit(3).order(created_at: :desc)
+    posts.where(author_id: id).limit(3).order(created_at: :desc)
   end
 end
