bd959bfe333fa573501888e23ad1b5ffc672dcf3
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-05-19 09:32:37 -0600

    Make private update counter methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,10 @@ class Comment < ApplicationRecord
 
   after_create :update_comments_counter
 
+  private
+
   def update_comments_counter
     post.increment!(:comments_counter)
   end
+  
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -7,4 +7,5 @@ class Like < ApplicationRecord
   def update_likes_counter
     post.increment!(:likes_counter)
   end
+  
 end
